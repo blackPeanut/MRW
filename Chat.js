8368ee401fbc83628498bf1eedfff2807c6acd2a
blackPeanut <daniel@galt.hk>
2018-04-20 14:37:40 +0300

    step #3. Chat without backend

--- Chat.js
+++ Chat.js
@@ -1,25 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import {
   View,
   Text,
   StyleSheet,
-} from 'react-native'
+} from 'react-native';
+
+import { GiftedChat } from 'react-native-gifted-chat';
 
 export default class Chat extends React.Component {
+  state = {
+    messages: [{
+      _id: 1,
+      text: 'Привет! Добро пожаловать на воркшоп в Mail.Ru',
+      createdAt: new Date(),
+      user: {
+        _id: 2,
+        name: 'MRG',
+        avatar: 'https://ict2go.ru/uploads/media/events_lid_image/0001/06/thumb_5562_events_lid_image_small.png'
+      }
+    }]
+  }
+
+  onSend = (messages = []) => {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+
   render () {
     return (
-      <View style={styles.container}>
-        <Text>Chat</Text>
-      </View>
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+        listViewProps={{style: styles.chatContainer}}
+        user={{
+          _id: 1,
+          name: 'Daniel'
+        }}
+      />
     )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+  chatContainer: {
+    backgroundColor: 'white'
   }
 })
-
